5615511177db2a7336f44e898814f97d709bd1a8
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-29 21:27:25 +0200

    css

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'php/Database.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fun Fair - Back Office</title>
-    <link rel="stylesheet" href="public/assets/css/back.css">
+    <link rel="stylesheet" href="css/back.css">
 </head>
 <body>
     <div class="container">
