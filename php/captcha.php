60009c11ea37e0ec975839348047b325dfc9616f
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-29 21:56:31 +0200

    commit

--- php/captcha.php
+++ php/captcha.php
@@ -25,7 +25,7 @@ $stmt = $dbh->query($sql); // Exécuter la requête
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/captcha.css">
-    <link rel="shortcut icon" href="asset/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../asset/logo.png" type="image/x-icon">
     <title>Captcha</title>
 </head>
 <body>
