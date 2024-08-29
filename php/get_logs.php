5d0908129b9305d8d3f536f0a30b8df7c456e0d6
Titouan <titouan.melchio@gmail.com>
2024-08-29 23:17:38 +0200

    logs la c bon ?

--- php/get_logs.php
+++ php/get_logs.php
@@ -1,4 +1,3 @@
-
 <?php
 // Activer l'affichage des erreurs pour le débogage
 ini_set('display_errors', 1);
@@ -6,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Inclure le fichier de connexion à la base de données
-include __DIR__ . '/Database.php'; // Chemin absolu pour inclure db.php
+include __DIR__ . '/db.php'; // Chemin absolu pour inclure db.php
 
 // Vérifier la connexion à la base de données
 if (!isset($dbh)) {
