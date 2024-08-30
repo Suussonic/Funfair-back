c29d3fe871f370c6643a21541108def68e673e4b
mancho1512 <borgesazerty@gmail.com>
2024-08-30 22:57:58 +0200

    Update attraction.php

--- php/attraction.php
+++ php/attraction.php
@@ -1,1 +1,13 @@
+<?php
+session_start();
+include 'Database.php'; 
 
+// Gérer la demande de suppression
+if (isset($_POST['delete_id'])) {
+    $delete_id = $_POST['delete_id'];
+    $delete_sql = "DELETE FROM captcha WHERE id = :id";
+    $stmt = $dbh->prepare($delete_sql);
+    $stmt->execute([':id' => $delete_id]);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
