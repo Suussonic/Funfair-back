df7d037f9da0cec3c79bf9e775c4463aa37a5338
mancho1512 <borgesazerty@gmail.com>
2024-08-30 14:06:38 +0200

    Update user.php

--- php/user.php
+++ php/user.php
@@ -1,4 +1,3 @@
-<?php
 session_start();
 include_once('Database.php');
 
@@ -13,16 +12,15 @@ if (isset($_POST['delete_id'])) {
 }
 
 // Gérer la demande d'ajout d'utilisateur
-if (isset($_POST['add_lastname']) && isset($_POST['add_firstname']) && isset($_POST['add_gender']) && isset($_POST['add_email']) && isset($_POST['add_confirm']) && isset($_POST['add_cle']) && isset($_POST['add_role'])) {
+if (isset($_POST['add_lastname']) && isset($_POST['add_firstname']) && isset($_POST['add_gender']) && isset($_POST['add_email']) && isset($_POST['add_confirm']) && isset($_POST['add_cle'])) {
     $lastname = $_POST['add_lastname'];
     $firstname = $_POST['add_firstname'];
     $gender = $_POST['add_gender'];
     $email = $_POST['add_email'];
     $confirm = $_POST['add_confirm'];
     $cle = $_POST['add_cle'];
-    $role = $_POST['add_role'];
     
-    $insert_sql = "INSERT INTO users (lastname, firstname, gender, email, confirm, cle, role) VALUES (:lastname, :firstname, :gender, :email, :confirm, :cle, :role)";
+    $insert_sql = "INSERT INTO users (lastname, firstname, gender, email, confirm, cle) VALUES (:lastname, :firstname, :gender, :email, :confirm, :cle)";
     $stmt = $dbh->prepare($insert_sql);
     $stmt->execute([
         ':lastname' => $lastname, 
@@ -30,15 +28,14 @@ if (isset($_POST['add_lastname']) && isset($_POST['add_firstname']) && isset($_P
         ':gender' => $gender, 
         ':email' => $email, 
         ':confirm' => $confirm, 
-        ':cle' => $cle, 
-        ':role' => $role
+        ':cle' => $cle
     ]);
     header("Location: " . $_SERVER['PHP_SELF']);
     exit;
 }
 
 // Gérer la demande de modification d'utilisateur
-if (isset($_POST['edit_id']) && isset($_POST['edit_lastname']) && isset($_POST['edit_firstname']) && isset($_POST['edit_gender']) && isset($_POST['edit_email']) && isset($_POST['edit_confirm']) && isset($_POST['edit_cle']) && isset($_POST['edit_role'])) {
+if (isset($_POST['edit_id']) && isset($_POST['edit_lastname']) && isset($_POST['edit_firstname']) && isset($_POST['edit_gender']) && isset($_POST['edit_email']) && isset($_POST['edit_confirm']) && isset($_POST['edit_cle'])) {
     $edit_id = $_POST['edit_id'];
     $edit_lastname = $_POST['edit_lastname'];
     $edit_firstname = $_POST['edit_firstname'];
@@ -46,9 +43,8 @@ if (isset($_POST['edit_id']) && isset($_POST['edit_lastname']) && isset($_POST['
     $edit_email = $_POST['edit_email'];
     $edit_confirm = $_POST['edit_confirm'];
     $edit_cle = $_POST['edit_cle'];
-    $edit_role = $_POST['edit_role'];
 
-    $update_sql = "UPDATE users SET lastname = :lastname, firstname = :firstname, gender = :gender, email = :email, confirm = :confirm, cle = :cle, role = :role WHERE id = :id";
+    $update_sql = "UPDATE users SET lastname = :lastname, firstname = :firstname, gender = :gender, email = :email, confirm = :confirm, cle = :cle WHERE id = :id";
     $stmt = $dbh->prepare($update_sql);
     $stmt->execute([
         ':lastname' => $edit_lastname, 
@@ -57,7 +53,6 @@ if (isset($_POST['edit_id']) && isset($_POST['edit_lastname']) && isset($_POST['
         ':email' => $edit_email, 
         ':confirm' => $edit_confirm, 
         ':cle' => $edit_cle, 
-        ':role' => $edit_role, 
         ':id' => $edit_id
     ]);
     header("Location: " . $_SERVER['PHP_SELF']);
@@ -65,6 +60,6 @@ if (isset($_POST['edit_id']) && isset($_POST['edit_lastname']) && isset($_POST['
 }
 
 // Récupérer les utilisateurs de la base de données
-$sql = "SELECT id, lastname, firstname, gender, email, confirm, cle, role FROM users";
+$sql = "SELECT id, lastname, firstname, gender, email, confirm, cle FROM users";
 $stmt = $dbh->query($sql);
 ?>
