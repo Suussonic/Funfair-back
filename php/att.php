b588c4c89c902e2aeaefa0291671071d2af4bd5d
mancho1512 <borgesazerty@gmail.com>
2024-08-30 23:18:35 +0200

    Update att.php bonjour

--- php/att.php
+++ php/att.php
@@ -1,5 +1,10 @@
-<?php
-session_start();
-include 'Database.php';
-
-
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Bonjour</title>
+</head>
+<body>
+    <p>Bonjour</p>
+</body>
+</html>
