e61b75983c094fba997f56aa8738881f0cd16e60
Nathan <nathounng@gmail.com>
2024-09-01 23:43:09 +0200

    cherche vraiment

--- php/newsletter.php
+++ php/newsletter.php
@@ -1,10 +1,11 @@
 <?php
-include 'Database.php';
-include 'mailer.php';
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+include 'Database.php';
+include 'mailer.php';
+
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
