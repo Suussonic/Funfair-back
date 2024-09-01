4934627edc7d9610c9db73980710ec84779b5351
Titouan <titouan.melchio@gmail.com>
2024-09-01 21:17:51 +0200

    user4

--- php/users.php
+++ php/users.php
@@ -47,9 +47,9 @@ $stmt = $dbh->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/users.css">
+    <link rel="stylesheet" href="../css/captcha.css">
     <link rel="shortcut icon" href="../asset/logo.png" type="image/x-icon">
-    <title>Gestion utilisateur</title>
+    <title>Gestion des Utilisateurs</title>
 </head>
 <body>
 
