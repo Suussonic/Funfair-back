82e09bbb094a856b42ef746e9524d8233d68487f
mancho1512 <borgesazerty@gmail.com>
2024-09-01 21:09:48 +0200

    Update att.php

--- php/att.php
+++ php/att.php
@@ -11,7 +11,7 @@ $stmt = $dbh->query($sql);
 // Gérer la demande de suppression
 if (isset($_POST['delete_id'])) {
     $delete_id = $_POST['delete_id'];
-    $delete_sql = "DELETE FROM attraction WHERE id = :id";
+    $delete_sql = "DELETE FROM captcha WHERE id = :id";
     $stmt = $dbh->prepare($delete_sql);
     $stmt->execute([':id' => $delete_id]);
     header("Location: " . $_SERVER['PHP_SELF']);
