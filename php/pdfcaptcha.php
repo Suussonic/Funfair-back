4244fb9a145ea3d47c37649eea0c479bc4a25d4d
mancho1512 <borgesazerty@gmail.com>
2024-09-01 23:05:01 +0200

    Update pdfcaptcha.phpcommentaire

--- php/pdfcaptcha.php
+++ php/pdfcaptcha.php
@@ -3,13 +3,13 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Include the database connection file
+
 include_once('Database.php');
 
-// Include the FPDF library
+
 require('../fpdf186/fpdf.php');
 
-// Retrieve all information from the calculations table
+
 $sql = "SELECT id, q, r FROM captcha";
 
 try {
@@ -19,7 +19,7 @@ try {
     die("Error fetching data: " . $e->getMessage());
 }
 
-// Generate the PDF
+
 class PDF extends FPDF
 {
     function Header()
@@ -31,14 +31,14 @@ class PDF extends FPDF
 
     function captchaTable($header, $data)
     {
-        // Column widths
+        
         $w = array(10, 50, 30);
-        // Headers
+       
         for ($i = 0; $i < count($header); $i++) {
             $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C');
         }
         $this->Ln();
-        // Data
+      
         foreach ($data as $row) {
             $this->Cell($w[0], 6, $row['id'], 1);
             $this->Cell($w[1], 6, $row['q'], 1);
