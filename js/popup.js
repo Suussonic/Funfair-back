7ebbeb05726fff64920fbcb351e81d6baeb6496a
mancho1512 <borgesazerty@gmail.com>
2024-09-01 23:01:26 +0200

    Update popup.jscommentaire

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,14 @@
-// Ouvrir le pop-up pour ajouter un captcha
+
 function openAddPopup() {
     document.getElementById('addPopupForm').style.display = 'flex';
 }
 
-// Fermer le pop-up d'ajout
+
 function closeAddPopup() {
     document.getElementById('addPopupForm').style.display = 'none';
 }
 
-// Ouvrir le pop-up pour modifier un captcha avec les valeurs actuelles
+
 function openEditPopup(id, q, r) {
     document.getElementById('edit_id').value = id;
     document.getElementById('edit_q').value = q;
@@ -16,7 +16,7 @@ function openEditPopup(id, q, r) {
     document.getElementById('editPopupForm').style.display = 'flex';
 }
 
-// Fermer le pop-up de modification
+
 function closeEditPopup() {
     document.getElementById('editPopupForm').style.display = 'none';
 }
