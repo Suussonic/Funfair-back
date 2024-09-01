ec93cc8e55efa91c6aff152fd422f8519267e4da
mancho1512 <borgesazerty@gmail.com>
2024-09-01 19:43:39 +0200

    Update reservation.php

--- php/reservation.php
+++ php/reservation.php
@@ -63,52 +63,7 @@ $stmt = $dbh->query($sql);
     <a href="../index.php" class="action-button">Retour au Back</a>
 </div>
 
-<!-- Formulaire pour Ajouter une Réservation -->
-<div id="addPopupForm" class="popup">
-    <div class="popup-content">
-        <span class="close" onclick="closeAddPopup()">&times;</span>
-        <h2>Ajouter une Réservation</h2>
-        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <label for="add_attractionid">Attraction ID:</label>
-            <input type="number" name="add_attractionid" required>
-            <label for="add_montant">Montant:</label>
-            <input type="number" name="add_montant" required>
-            <label for="add_quantity">Quantité:</label>
-            <input type="number" name="add_quantity" required>
-            <label for="add_date">Date:</label>
-            <input type="date" name="add_date" required>
-            <label for="add_heure">Heure:</label>
-            <input type="time" name="add_heure" required>
-            <label for="add_email">Email:</label>
-            <input type="email" name="add_email" required>
-            <button type="submit" class="action-button">Ajouter</button>
-        </form>
-    </div>
-</div>
 
-<!-- Formulaire pour Modifier une Réservation -->
-<div id="editPopupForm" class="popup">
-    <div class="popup-content">
-        <span class="close" onclick="closeEditPopup()">&times;</span>
-        <h2>Modifier une Réservation</h2>
-        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <input type="hidden" name="edit_id" id="edit_id">
-            <label for="edit_attractionid">Attraction ID:</label>
-            <input type="number" name="edit_attractionid" id="edit_attractionid" required>
-            <label for="edit_montant">Montant:</label>
-            <input type="number" name="edit_montant" id="edit_montant" required>
-            <label for="edit_quantity">Quantité:</label>
-            <input type="number" name="edit_quantity" id="edit_quantity" required>
-            <label for="edit_date">Date:</label>
-            <input type="date" name="edit_date" id="edit_date" required>
-            <label for="edit_heure">Heure:</label>
-            <input type="time" name="edit_heure" id="edit_heure" required>
-            <label for="edit_email">Email:</label>
-            <input type="email" name="edit_email" id="edit_email" required>
-            <button type="submit" class="action-button">Modifier</button>
-        </form>
-    </div>
-</div>
 
 <script src="../js/popup.js"></script>
 </body>
